2dc4f49eb082239378621678167b69446a1d59cb
Ddol-bae <41590760+Ddol-bae@users.noreply.github.com>
2021-09-09 02:20:47 +0900

    Jump
    걷기 애니메이션 + 점프(레이캐스트)
    //점프 애니메이션 FALSE로 전환이 안되는 문제

--- 2DGame/Assets/Scripts/Player_move.cs
+++ 2DGame/Assets/Scripts/Player_move.cs
@@ -5,14 +5,39 @@ using UnityEngine;
 public class Player_move : MonoBehaviour
 {
     public float MaxSpeed;
+    public float JumpPower;
     Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
+    Animator animator;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
-
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    
     }
 
+    void Update() //단발적인 입력에 좋음.
+    {   
+        //Sprite Direction Switch
+        if(Input.GetButtonDown("Horizontal"))
+            spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
+
+        //Walk Animation
+        if(Mathf.Abs(rigid.velocity.x) < 0.3)
+            animator.SetBool("IsWalking", false);
+        else
+            animator.SetBool("IsWalking", true);
+
+        //Jump
+        if(Input.GetButtonDown("Jump"))
+            rigid.AddForce(Vector2.up * JumpPower, ForceMode2D.Impulse);
+            animator.SetBool("IsJump", true);
+        
+    }
+
+    
     void FixedUpdate()
     {
         //Player Move
@@ -24,6 +49,13 @@ public class Player_move : MonoBehaviour
             rigid.velocity = new Vector2(MaxSpeed, rigid.velocity.y);
         else if(rigid.velocity.x < MaxSpeed*(-1)) //Left
             rigid.velocity = new Vector2(MaxSpeed*(-1), rigid.velocity.y);
-        
+
+        //Landing Platform
+        Debug.DrawRay(rigid.position, Vector3.down, new Color(0,1,0));
+        RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
+        if(rayHit.collider != null) {
+            if(rayHit.distance < 1f)
+                animator.SetBool("IsJump", false);
+        }
     }
 }
