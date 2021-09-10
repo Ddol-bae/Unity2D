bb217cab3d2e6e341bea5fb4066cf5d5bf134a34
Ddol-bae <41590760+Ddol-bae@users.noreply.github.com>
2021-09-10 15:27:10 +0900

    Monster AI
    몬스터 움직임 개선

--- 2DGame/Assets/Scripts/Monster_Move.cs
+++ 2DGame/Assets/Scripts/Monster_Move.cs
@@ -6,12 +6,15 @@ public class Monster_Move : MonoBehaviour
 {
     Rigidbody2D rigid;
     public int NextMove;
+    Animator animator;
+    SpriteRenderer spriteRenderer;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
-        Think();
-
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+        
         Invoke("Think", 3);
     }
 
@@ -21,21 +24,33 @@ public class Monster_Move : MonoBehaviour
         rigid.velocity = new Vector2(NextMove, rigid.velocity.y);
 
         // PlatForm_Check
-        Vector2 FrontVector = new Vector2(rigid.position.x + NextMove, rigid.position.y);
-        Debug.DrawRay(FrontVector, Vector3.down, new Color(0,1,0));
+        Vector2 FrontVector = new Vector2(rigid.position.x + NextMove*0.3f, rigid.position.y);
+        Debug.DrawRay(FrontVector, Vector3.down, Color.red);
         RaycastHit2D rayHit = Physics2D.Raycast(FrontVector, Vector3.down, 1, LayerMask.GetMask("Platform"));
-        if(rayHit.collider == null) {
-            Debug.Log("낭떠러지");
-            
-        }
+        if(rayHit.collider == null)          
+            turn();
     }
 
     void Think()
     {   //Random Move
         NextMove = Random.Range(-1, 2);
 
-        Invoke("Think", 3);
+        animator.SetInteger("WalkSpeed", NextMove);
 
+        if(NextMove != 0)
+            spriteRenderer.flipX = NextMove == 1;
+        
+        Invoke("Think", 3);
+    }
+
+    void turn()
+    {
+        NextMove *= -1;
+        spriteRenderer.flipX = NextMove == 1;
+
+        CancelInvoke();
+        Invoke("Think", 3);
     }
 }
+
  
