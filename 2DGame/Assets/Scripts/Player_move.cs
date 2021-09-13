86210837f9e1d32b4b4ccfd972b4448a51f2228a
Ddol-bae <41590760+Ddol-bae@users.noreply.github.com>
2021-09-13 18:55:22 +0900

    피격 이벤트 추가

--- 2DGame/Assets/Scripts/Player_move.cs
+++ 2DGame/Assets/Scripts/Player_move.cs
@@ -61,8 +61,31 @@ public class Player_move : MonoBehaviour
     
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Enemy") {
-            Debug.Log("플레이어가 맞았습니다");
-        }
+        if(collision.gameObject.tag == "Enemy") 
+            OnDamaged(collision.transform.position);   
+        
     }
+
+    void OnDamaged(Vector2 targetPos)
+
+    {   //Change layer
+        gameObject.layer = 8;
+
+        //View Alpha
+        spriteRenderer.color = new Color(1,1,1,0.4f);
+
+        //Reaction Force
+        int dirc = transform.position.x - targetPos.x > 0 ? 1 : -1;
+        rigid.AddForce(new Vector2(dirc,1)*7 ,ForceMode2D.Impulse);
+
+        Invoke("OffDamaged", 2);
+
+    }
+
+    void OffDamaged()
+    {
+        gameObject.layer = 3; 
+        spriteRenderer.color = new Color(1,1,1,1);
+    }
+
 }
